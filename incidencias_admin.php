48c284e708cd295bf7d8809f066442b410eac3fe
AaronOT <55807823+AaronOT@users.noreply.github.com>
2019-11-06 19:17:32 +0100

    Solución de problemas en incidencias_admin

--- incidencias_admin.php
+++ incidencias_admin.php
@@ -16,7 +16,7 @@
 
 
 		
-
+		//Muestra todas las incidencias creadas
 			echo "<h2>Incidencias</h2>";
 			while ($row=mysqli_fetch_array($result)) {
 
@@ -29,13 +29,13 @@
 				$row2=mysqli_fetch_array($result2);
 
         		$nom=$row2['nombre_recurso'];    
+        		// Dependiendo de si la incidencia esta solucionada o no, en el caso de que este solucionada te la muestra en verde y no aparece el boton de solucionar,en caso contrario aparece en rojo y aparece el boton de solucionar
+        		if ($row['estado']==1){
+        		echo "<h4 style='color:#47CF30;'>$nom ---- ".$row['dsc_incidencia']."";
 
-        		echo "$nom ---- ";
-
-				echo " ".$row['dsc_incidencia']."";
-				echo "<a href='$solucion_in'> Solucionar </a>";
-
-
+			}else if ($row['estado']==0){
+				echo "<h4 style='color:#F00F0F;'>$nom ---- ".$row['dsc_incidencia']."<a href='$solucion_in'> Solucionar </a></h4>";
+			}
 
 			}
 		
